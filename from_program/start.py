a7dadba9aa731449ea4092a38aae36736f48ee56
Felix Finkbeiner <fe_fi@gmx.net>
2018-08-17 22:17:12 +0200

    fixed Unknown naming and added list ui (but not finished)

--- from_program/start.py
+++ from_program/start.py
@@ -1,5 +1,6 @@
 import cv2
 from recognition import Recognition
+from ui import Ui
 
 
 def main():
@@ -8,10 +9,14 @@ def main():
     known_face_names = []
 
     rec = Recognition()
+    ui = Ui()
 
     while True:
         #print("length: "+ str(len(known_face_names)))
         rec.find_faces(known_face_encodings, known_face_names)
+
+        ui.setList(known_face_names)
+
         # Hit 'q' on the keyboard to quit!
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
--- from_program/ui.py
+++ from_program/ui.py
@@ -2,40 +2,24 @@ import tkinter
 from tkinter import *
 from tkinter import ttk
 
+class Ui:
 
-root = Tk()
-root.title("Known names")
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Known names")
 
-listbox = Listbox(root)
-listbox.pack()
+        self.listbox = Listbox(self.root)
+        self.listbox.pack()
+
+        #dTODO detach 
+        self.root.mainloop()
+
+    def setList(self, list): #TODO besser machen (nur updates)
+        self.listbox.delete(0, END) #clear
+        for item in list:
+            self.listbox.insert(END, item)
 
 #listbox.insert(END, "a list entry")
 
 #for item in ["one", "two", "three", "four"]:
 #    listbox.insert(END, item)
-
-
-#mainframe = ttk.Frame(root, padding="3 3 12 12")
-#mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-#mainframe.columnconfigure(0, weight=1)
-#mainframe.rowconfigure(0, weight=1)
-
-#feet = StringVar()
-#meters = StringVar()
-
-#feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-#feet_entry.grid(column=2, row=1, sticky=(W, E))
-
-#ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-#ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
-
-#ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-#ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-#ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-#for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
-
-#feet_entry.focus()
-#root.bind('<Return>', calculate)
-
-root.mainloop()
